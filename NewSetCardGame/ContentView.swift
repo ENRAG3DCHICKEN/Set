188ed752226ce321803f3abb42617419b2ddbd92
CS193P student <yiu.jackie@gmail.com>
2020-08-14 13:10:52 -0400

    Initial Commit

---
+++ NewSetCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewSetCardGame
+//
+//  Created by ENRAG3DCHICKEN on 2020-08-14.
+//  Copyright © 2020 ENRAG3DCHICKEN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewSetCardGameTests/NewSetCardGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewSetCardGameTests.swift
+//  NewSetCardGameTests
+//
+//  Created by ENRAG3DCHICKEN on 2020-08-14.
+//  Copyright © 2020 ENRAG3DCHICKEN. All rights reserved.
+//
+
+import XCTest
+@testable import NewSetCardGame
+
+class NewSetCardGameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
